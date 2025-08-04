88eb327e3852e1413fea88c2e5c2c37f5e162e23
Deepak19061999 <deepakrpaul1999@gmail.com>
2025-08-04 12:09:35 +0530

    completed

---
+++ src/test/java/org/gitpack/GitClass.java
@@ -0,0 +1,11 @@
+package org.gitpack;
+
+public class GitClass {
+	
+	public static void main(String[] args) {
+		System.out.println("Day 1");
+		System.out.println("Day 2");
+		System.out.println("Day 3");
+	}
+
+}
