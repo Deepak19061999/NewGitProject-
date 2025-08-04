e463ba3f045aa98fdfa28dd766a6c4012e9e4dfd
Deepak19061999 <deepakrpaul1999@gmail.com>
2025-08-04 12:15:40 +0530

    Updated

--- src/test/java/org/gitpack/GitClass.java
+++ src/test/java/org/gitpack/GitClass.java
@@ -4,8 +4,9 @@ public class GitClass {
 	
 	public static void main(String[] args) {
 		System.out.println("Day 1");
-		System.out.println("Day 2");
-		System.out.println("Day 3");
+		System.out.println("Deepak Changes the code ");
+		System.out.println("Verify the change");
+		System.out.println("Extra line");
 	}
 
 }
